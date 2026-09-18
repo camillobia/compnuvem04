f921c81081fc231e29ddf52a78ad0b361fe00bac
camillobia <beatriz.lombardo2002@gmail.com>
2026-09-18 20:35:14 -0300

    Compnuvem

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main { 
+    public static void main(String[] args) { 
+    System.out.println("====================================");
+    System.out.println("Aplicacao: Minha Primeira Aplicacao Java com Docker"); 
+    System.out.println("Aluno: Beatriz Lombardo Camillo"); 
+    System.out.println("Disciplina: Computacao em Nuvem"); 
+    System.out.println("Mensagem: Este programa esta executando dentro de um container Docker."); 
+    System.out.println("===================================="); 
+    } 
+}
